b774e71b194871a98e3dd623f93ca68727a4401e
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-10-02 17:02:37 -0300

    add zod in project

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -4,8 +4,21 @@ import {
   HttpCode,
   Post,
   ConflictException,
+  UsePipes,
 } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { hash } from 'bcryptjs'
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+const createAccountBodySchema = z.object({
+  // here we let's create the schema for create the validation
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema> // we can our schema for create the type of body
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -13,7 +26,8 @@ export class CreateAccountController {
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema)) // now the values received in the body of request will be validated
+  async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
@@ -28,11 +42,13 @@ export class CreateAccountController {
       )
     }
 
+    const hashPassword = await hash(password, 8) // '8' is the number of rounds for create hash (how repeat creation of hash 8 times)
+
     await this.prisma.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashPassword,
       },
     })
   }
---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,23 @@
+import { PipeTransform, BadRequestException } from '@nestjs/common'
+import { ZodSchema, ZodError } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      this.schema.parse(value) // Here the validation will be done
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          errors: fromZodError(error),
+          message: 'Validation Failed',
+          statusCode: 400,
+        })
+      }
+      throw new BadRequestException('Validation failed')
+    }
+    return value
+  }
+}
