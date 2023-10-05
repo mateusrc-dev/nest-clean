3f306ff3a049b090c7630289cf753ccce166155d
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-10-05 18:05:06 -0300

    create controller for fetch recent questions

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,16 +1,53 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { TokenPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  // here we let's create the schema for create the validation
+  title: z.string(),
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema> // we can our schema for create the type of body
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
-  @Post()
-  async handle(@CurrentUser() user: TokenPayload) {
-    console.log(user.sub)
+  @Post() // decorator is like a function, the elements after it are arguments
+  async handle(
+    @Body(bodyValidationPipe)
+    body: CreateQuestionBodySchema, // doing the validate of request body
+    @CurrentUser() user: TokenPayload, // get the user logged
+  ) {
+    const { title, content } = body
+    const { sub: userId } = user
+    const slug = this.convertToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    })
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, '-')
+      .replace(/^-+|-+$/g, '')
+      .trim()
   }
 }
---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Query } from '@nestjs/common'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const pageQueryParamSchema = z // remember that this is a schema (representation of data)
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema> // creating a type
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema) // pipe that we create to do validation of schema
+
+@Controller('/questions')
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get() // decorator is like a function, the elements after it are arguments
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
+    const perPage = 1
+
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc', // to get the questions more recent
+      },
+    })
+
+    return { questions }
+  }
+}
