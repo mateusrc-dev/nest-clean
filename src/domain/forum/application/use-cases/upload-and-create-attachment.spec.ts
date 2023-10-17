654d1021bafb12f182ab8e09f0058cfdd88ac799
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-10-17 16:00:51 -0300

    updated project

---
+++ src/domain/forum/application/use-cases/upload-and-create-attachment.spec.ts
@@ -0,0 +1,49 @@
+import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments-repository'
+import { UploadAndCreateAttachmentUseCase } from './upload-and-create-attachment'
+import { FakeUploader } from 'test/storage/faker-uploader'
+import { InvalidAttachmentType } from './errors/invalid-attachment-type'
+
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
+let fakeUploader: FakeUploader
+let sut: UploadAndCreateAttachmentUseCase // sut = system under test
+describe('Upload and create attachment', () => {
+  beforeEach(() => {
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
+    fakeUploader = new FakeUploader()
+
+    sut = new UploadAndCreateAttachmentUseCase(
+      inMemoryAttachmentsRepository,
+      fakeUploader,
+    )
+  })
+
+  it('should be able to upload and create an attachment', async () => {
+    const result = await sut.execute({
+      fileName: 'profile.png',
+      fileType: 'image/png',
+      body: Buffer.from(''),
+    })
+
+    expect(result.isRight()).toBe(true) // id cannot be undefined, null
+    expect(result.value).toEqual({
+      attachment: inMemoryAttachmentsRepository.items[0],
+    })
+    expect(fakeUploader.uploads).toHaveLength(1)
+    expect(fakeUploader.uploads[0]).toEqual(
+      expect.objectContaining({
+        fileName: 'profile.png',
+      }),
+    )
+  })
+
+  it('should not be able upload an attachment with invalid file type', async () => {
+    const result = await sut.execute({
+      fileName: 'profile.mp3',
+      fileType: 'audio/mpeg',
+      body: Buffer.from(''),
+    })
+
+    expect(result.isLeft()).toBe(true) // id cannot be undefined, null
+    expect(result.value).toBeInstanceOf(InvalidAttachmentType)
+  })
+})
