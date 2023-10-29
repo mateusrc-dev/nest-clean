bf47122b026c4b0b0c8c7333f2d9a8143c03e94f
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-10-29 19:01:22 -0300

    updated project

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -3,19 +3,28 @@ import { randomUUID } from 'node:crypto'
 import { config } from 'dotenv' // for have access the environment variable
 import { execSync } from 'node:child_process'
 import { DomainEvents } from '@/core/events/domain-events'
+import { Redis } from 'ioredis'
+import { envSchema } from '@/infra/env/env'
 
 config({ path: '.env', override: true }) // config loading the environment variable and we can put configs
 // 'override' tells nest to override the variable when this variable has been defined in the past
 config({ path: '.env.test', override: true })
 
+const env = envSchema.parse(process.env)
+
 const prisma = new PrismaClient()
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+})
 
 function generateUniqueDataBaseUrl(schemaId: string) {
-  if (!process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error('Please provider a DATABASE_URL environment variable')
   }
 
-  const url = new URL(process.env.DATABASE_URL) // create a url object with methods
+  const url = new URL(env.DATABASE_URL) // create a url object with methods
 
   url.searchParams.set('schema', schemaId)
   // here we let's change value of query param for a schemaId - will be create a isolated database for test
@@ -31,6 +40,8 @@ beforeAll(async () => {
   process.env.DATABASE_URL = databaseURL // we let's overwrite the environment variable with new database url
 
   DomainEvents.shouldRun = false
+
+  await redis.flushdb()
 
   // execSync - for execute a command in code
   execSync('npx prisma migrate deploy') // deploy will be for run the migrations without verify if have new schemas - creating new schema for test
